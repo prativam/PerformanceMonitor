9ebcaacb40bd5e6aaf76dca54ff291084a7821ad
prativam <1991prativa@gmail.com>
2021-10-28 02:01:28 -0500

    Update routes.js
    Implemented Max Aggregation in API

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+const { Console } = require("console");
 const express = require("express")
 const Performance = require("./models/Performance")
 const router = express.Router()
@@ -18,17 +19,35 @@ router.post("/import", async (req, res) => {
 
 router.get("/analytics", async (req, res) => {
     try {
-        
-        var dateArr  = req.query.date.split(',')
-        let metric = req.query.metric
-        let Aggr = req.query.Aggr
+
+        var dateArr = req.query.date.split(',')
+        var start = new Date(dateArr[0])
+        var end = new Date(dateArr[1])
+        var metric = req.query.metric
+        var Aggr = req.query.Aggr
+        console.log(Aggr)
+        var pipeline = [
+            {
+                $match: {
+                    
+                    "CounterName":metric,
+                     "date":{
+                        $gte: start,
+                        $lte: end
+                    }
+                }
+            },
+            {
+              $group: {
+                _id: Aggr,
+                max: { $max: '$'+Aggr}
+              }
+            }
+          ]
+        var aggregatefun = await Performance.aggregate(pipeline);
+        console.log(aggregatefun)
        
-        var data = await  Performance.find({ 'CounterName':metric}).exec()
-        var data = await  Performance.find({ 'CounterName':metric, 'date': {
-                $gte: dateArr[0],
-               $lte: dateArr[1]
-             } }).exec()
-        res.status(200).send(data)
+        res.status(200).send(aggregatefun)
     } catch (err) {
         console.error(err)
         res.status(500).send('Invalid Data')
@@ -37,17 +56,19 @@ router.get("/analytics", async (req, res) => {
 
 router.get("/badperformance", async (req, res) => {
     try {
-        
-        var dateArr  = req.query.date.split(',')
+
+        var dateArr = req.query.date.split(',')
         let metric = req.query.metric
         let p90 = req.query.p90
-       
-        var data = await  Performance.find({ 'CounterName':metric}).exec()
-        var data = await  Performance.find({ 'CounterName':metric, 'date': {
+
+        var data = await Performance.find({ 'CounterName': metric }).exec()
+        var data = await Performance.find({
+            'CounterName': metric, 'date': {
                 $gte: dateArr[0],
-               $lte: dateArr[1]
-             },
-             'P90':{$gte : p90 }}).exec()
+                $lte: dateArr[1]
+            },
+            'P90': { $gte: p90 }
+        }).exec()
         res.status(200).send(data)
     } catch (err) {
         console.error(err)
